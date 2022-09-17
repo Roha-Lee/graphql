c6af36f90f71c5825dc988ce7c523dd558a7b7bb
Roha-Lee <roha.lee@buzzvil.com>
2022-09-18 00:41:46 +0900

    Feat: Add mutation

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ const typeDefs = gql`
         allTweets: [Tweet]
         tweet(id: ID): Tweet
     }
+    type Mutation {
+        postTweet(text: String, userId: ID): Tweet
+    }
 `;
 
 const resolvers = {
