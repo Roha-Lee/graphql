1833ea3e2e77ac5b8c0b97b92a1de765d3a0cf4b
Roha-Lee <roha.lee@buzzvil.com>
2022-09-18 00:33:51 +0900

    Feat: Tweet data schema

--- index.js
+++ index.js
@@ -1,61 +1,28 @@
-import { ApolloServer, gql } from 'apollo-server-express';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-import express from 'express';
-import http from 'http';
-import { readFileSync } from 'fs';
-import { join, dirname } from 'path';
+import { ApolloServer, gql } from 'apollo-server';
 
 const typeDefs = gql`
+    type User {
+        id: ID
+        username: String
+    }
+    type Tweet {
+        id: ID
+        text: String
+        author: User
+    }
     type Query {
-        "A simple type for getting started!"
-        hello: String
-        books: [ Book ]
-        people: [ Person ]
-    }
-    type Book {
-        title: String
-        author: Person
-        bookId: Int
-        url: String
-        message: String
-    }
-    type Person {
-        name: String
-        age: Int
-        id: Int
+        allTweets: [Tweet]
+        tweet(id: ID): Tweet
     }
 `;
 
 const resolvers = {
     Query: {
-        hello: () => "world",
-        books: () => {
-            return JSON.parse(readFileSync(join(dirname("."), "books.json").toString()));
-        },
-        people: () => {
-            return JSON.parse(readFileSync(join(dirname("."), "people.json").toString()));
-        }
     }
 }
 
-async function startApolloServer(typeDefs, resolvers) {
-  const app = express();
+const server = new ApolloServer({ typeDefs });
 
-  const httpServer = http.createServer(app);
-
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
-
-  await server.start();
-
-  server.applyMiddleware({ app });
-
-  await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
-
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
-};
-
-startApolloServer(typeDefs, resolvers);
+server.listen().then(({ url }) => {
+    console.log(`Running on ${url}`);
+})
