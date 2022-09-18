f4199f18af5065dbf16722e77073e68a0f12dfbb
Roha-Lee <roha.lee@buzzvil.com>
2022-09-18 22:49:27 +0900

    Add first resolver

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import { ApolloServer, gql } from 'apollo-server';
+const tweets = [
+    {
+        id: 1,
+        text: "1st tweet."
+    },
+    {
+        id: 1,
+        text: "2nd tweet."
+    },
+];
 
 const typeDefs = gql`
     type User {
@@ -24,10 +34,13 @@ const typeDefs = gql`
 
 const resolvers = {
     Query: {
+        allTweets() {
+            return tweets;
+        }
     }
 }
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
     console.log(`Running on ${url}`);
